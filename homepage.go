c787128c5d2db2ffb8f282200dbdfdf6da82d123
Quinn, Frank <Francis.Quinn@fmr.com>
2017-12-13 14:57:31 +0000

    added test for UI-mode

--- homepage.go
+++ homepage.go
@@ -2,17 +2,20 @@ package main
 
 import (
 "net/http"
-	"github.com/go-chi/chi"
+"github.com/go-chi/chi"
 )
 
-func startUiMode()  {
+const contentTypeHeader = `Content-Type`
+const htmlType = `text/html`
 
+func startUiMode() *chi.Mux{
 	r := chi.NewRouter()
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./ui/index.html")
+		w.Header().Set(contentTypeHeader, htmlType)
+
 	})
 	r.HandleFunc("/post", jsonHandler)
 	http.ListenAndServe(":9191", r)
+	return r
 }
-
-
