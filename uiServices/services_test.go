9ffe02e143b3c5ce16db7d443c696cb973e99aef
DMN1\a615194 <angela.rooney@fmr.com>
2017-12-18 15:34:57 +0000

    change path

--- uiServices/services_test.go
+++ uiServices/services_test.go
@@ -3,13 +3,17 @@ package services
 import (
 	"testing"
 	"github.com/stretchr/testify/assert"
+	"os"
 )
 
+
+
 func TestFilePathExist(t *testing.T) {
-	path := "C:/Users/a615194/go/src/github.com/xtracdev/automated-perf-test"
+	path := os.Getenv("GOPATH")
 	actual := false
 	actual = FilePathExist(path)
 	expected := true
+	os.Getenv("GOPATH")
 	assert.Equal (t, expected, actual)
 }
 
@@ -19,3 +23,12 @@ func TestFilePathDoesNotExist(t *testing.T) {
 	expected := false
 	assert.Equal (t, expected, actual)
 }
+
+//
+//func TestFileSave(t *testing.T) {
+//
+//	path :=  "C:/Users/a615194/go/src/github.com/xtracdev/automated-perf-test/config/ConfigTest.xml"
+//	actual := FilePathExist(path)
+//	expected := false
+//	assert.Equal (t, expected, actual)
+//}
