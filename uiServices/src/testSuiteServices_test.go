be75f0fbf35aaf66cd7526163e8c92a51c72aaaf
DMN1\a615194 <angela.rooney@fmr.com>
2018-02-05 15:41:39 +0000

    put working, unit test passing

--- uiServices/src/testSuiteServices_test.go
+++ uiServices/src/testSuiteServices_test.go
@@ -30,6 +30,27 @@ const validTestSuite = `
   ]
 }
 `
+
+const invalidTestSuite = `
+{
+  "name": 123,
+  "testStrategy": "SuiteBased",
+  "testCases": [
+    {
+      "name":"file1.xml",
+      "preThinkTime": "number",
+      "postThinkTime": 2000,
+      "execWeight": "Infrequent"
+    },
+    {
+      "name":"file2.xml",
+      "preThinkTime": 1,
+      "postThinkTime": 10,
+      "execWeight": "Sparse"
+    }
+  ]
+}
+`
 const TestSuiteMissingRequired = `
 {
   "testCases": [
@@ -187,4 +208,176 @@ func TestValidTestSuitePostConfigPathDirNotExist(t *testing.T) {
 
 	assert.Equal(t, w.Code, http.StatusBadRequest, "successfully created but should not have due to no config path specified")
 
+}
+
+
+
+func TestValidTestSuitePut(t *testing.T) {
+	r := chi.NewRouter()
+	r.Mount("/", GetIndexPage())
+
+	reader := strings.NewReader(validTestSuite)
+	r.HandleFunc("/test-suites", putTestSuites)
+
+	filePath := os.Getenv("GOPATH") + "/src/github.com/xtracdev/automated-perf-test/uiServices/test/"
+	request, err := http.NewRequest(http.MethodPut, "/test-suites/TestSuiteService.xml", reader)
+	request.Header.Set("configPathDir", filePath)
+
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, request)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	assert.Equal(t, w.Code, http.StatusNoContent, "Did Not successfully Update")
+}
+
+func TestTestSuiteMissingFieldPut(t *testing.T) {
+	r := chi.NewRouter()
+	r.Mount("/", GetIndexPage())
+
+	reader := strings.NewReader(TestSuiteMissingRequired)
+	r.HandleFunc("/test-suites", putTestSuites)
+
+	filePath := os.Getenv("GOPATH") + "/src/github.com/xtracdev/automated-perf-test/uiServices/test/"
+	request, err := http.NewRequest(http.MethodPut, "/test-suites/TestSuiteService.xml", reader)
+	request.Header.Set("configPathDir", filePath)
+
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, request)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	assert.Equal(t, w.Code, http.StatusBadRequest, "Sucessfully updated. Field Should be missing so update shouldn't occur")
+}
+
+
+func TestInvalidTestSuitePut(t *testing.T) {
+	r := chi.NewRouter()
+	r.Mount("/", GetIndexPage())
+
+	reader := strings.NewReader(invalidTestSuite)
+	r.HandleFunc("/test-suites", putTestSuites)
+
+	filePath := os.Getenv("GOPATH") + "/src/github.com/xtracdev/automated-perf-test/uiServices/test/"
+	request, err := http.NewRequest(http.MethodPut, "/test-suites/TestSuiteService.xml", reader)
+	request.Header.Set("configPathDir", filePath)
+
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, request)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	assert.Equal(t, w.Code, http.StatusBadRequest, "Sucessfully updated. Field data type should have been incorrect so update should occur")
+}
+
+
+func TestInvalidUrlTestSuitePut(t *testing.T) {
+	r := chi.NewRouter()
+	r.Mount("/", GetIndexPage())
+
+	reader := strings.NewReader(validTestSuite)
+	r.HandleFunc("/test-suites", putTestSuites)
+
+	filePath := os.Getenv("GOPATH") + "/src/github.com/xtracdev/automated-perf-test/uiServices/test/"
+	request, err := http.NewRequest(http.MethodPut, "/test-suites/xxx", reader)
+	request.Header.Set("configPathDir", filePath)
+
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, request)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	assert.Equal(t, w.Code, http.StatusNotFound, "Sucessfully updated. Should have have worked using /test-suites/xxx")
+}
+
+func TestNoUrlTestSuitePut(t *testing.T) {
+	r := chi.NewRouter()
+	r.Mount("/", GetIndexPage())
+
+	reader := strings.NewReader(validTestSuite)
+	r.HandleFunc("/test-suites", putTestSuites)
+
+	filePath := os.Getenv("GOPATH") + "/src/github.com/xtracdev/automated-perf-test/uiServices/test/"
+	request, err := http.NewRequest(http.MethodPut, "", reader)
+	request.Header.Set("configPathDir", filePath)
+
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, request)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	assert.Equal(t, w.Code, http.StatusNotFound, "Sucessfully updated. Should not have worked with no URL")
+}
+
+func TestPutWithNoPathSlash(t *testing.T) {
+	r := chi.NewRouter()
+	r.Mount("/", GetIndexPage())
+
+	reader := strings.NewReader(validTestSuite)
+	r.HandleFunc("/test-suites", putTestSuites)
+
+	filePath := os.Getenv("GOPATH") + "/src/github.com/xtracdev/automated-perf-test/uiServices/test"
+	request, err := http.NewRequest(http.MethodPut, "/test-suites/TestSuiteService.xml", reader)
+	request.Header.Set("configPathDir", filePath)
+
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, request)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	assert.Equal(t, w.Code, http.StatusNoContent, "Did not update. Should have added '/' to path")
+}
+
+func TestNoPathTestSuitePut(t *testing.T) {
+	r := chi.NewRouter()
+	r.Mount("/", GetIndexPage())
+
+	reader := strings.NewReader(validTestSuite)
+	r.HandleFunc("/test-suites", putTestSuites)
+
+	filePath := ""
+	request, err := http.NewRequest(http.MethodPut, "/test-suites/TestSuiteService.xml", reader)
+	request.Header.Set("configPathDir", filePath)
+
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, request)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	assert.Equal(t, w.Code, http.StatusBadRequest, "Successfully updated. Should not have worked due to no filepath")
+}
+
+func TestNoFileNameTestSuitePut(t *testing.T) {
+	r := chi.NewRouter()
+	r.Mount("/", GetIndexPage())
+
+	reader := strings.NewReader(validTestSuite)
+	r.HandleFunc("/test-suites", putTestSuites)
+
+	filePath := os.Getenv("GOPATH") + "/src/github.com/xtracdev/automated-perf-test/uiServices/test"
+	request, err := http.NewRequest(http.MethodPut, "/test-suites", reader)
+	request.Header.Set("configPathDir", filePath)
+
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, request)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	assert.Equal(t, w.Code, http.StatusBadRequest, "Successfully updated. Should not have worked due to no file name given")
 }
