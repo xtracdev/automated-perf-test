3caaa0cca1b81f115c3236c830c217bd5acdd3fb
a586754 <colm.cooney@fmr.com>
2018-02-05 11:22:46 +0000

    browse file on click

--- ui-src/src/app/configurations/configurations.component.ts
+++ ui-src/src/app/configurations/configurations.component.ts
@@ -5,6 +5,7 @@ import { JsonSchemaFormModule } from "angular2-json-schema-form";
 import { ToastsManager } from "ng2-toastr/ng2-toastr";
 import { ToastOptions } from "ng2-toastr/src/toast-options";
 import "rxjs/add/operator/map";
+import { DOCUMENT } from "@angular/platform-browser";
 
 @Component({
   selector: "app-configurations",
@@ -15,17 +16,15 @@ export class ConfigurationsComponent implements OnInit {
   formData = {};
   configPath = undefined;
   xmlFileName = undefined;
+  fileName = undefined;
   // needed for layout to load
   configSchema = { layout: "" };
-
-
 
   constructor(
     private automatedUIServices: AutomatedUIServices,
     private toastr: ToastsManager,
     private http: Http
   ) {}
-
 
   ngOnInit() {
     this.http
@@ -44,7 +43,6 @@ export class ConfigurationsComponent implements OnInit {
     };
   }
 
-
   onSubmit(configData) {
     this.automatedUIServices.postConfig$(configData, this.configPath).subscribe(
       data => {
@@ -62,35 +60,41 @@ export class ConfigurationsComponent implements OnInit {
             break;
           }
           case 400: {
-            this.toastr.error("Some of the Fields do not Conform to the Schema!", "An Error Occurred!");
+            this.toastr.error(
+              "Some of the Fields do not Conform to the Schema!",
+              "An Error Occurred!"
+            );
             break;
           }
           default: {
             this.toastr.error("Your Data did not Save!", "An Error Occurred!");
-
           }
         }
-
       }
     );
   }
 
   onCancel() {
-
     this.automatedUIServices
-    .getConfig$(this.configPath, this.xmlFileName)
-    .subscribe(
-      data => {
-        this.formData = data;
-        this.toastr.success("Previous Data Reloaded!");
-      },
-      error => {
-    this.configPath = undefined;
-    this.xmlFileName = undefined;
-    this.formData = undefined;
-      });
+      .getConfig$(this.configPath, this.xmlFileName)
+      .subscribe(
+        data => {
+          this.formData = data;
+          this.toastr.success("Previous Data Reloaded!");
+        },
+        error => {
+          this.configPath = undefined;
+          this.xmlFileName = undefined;
+          this.formData = undefined;
+        }
+      );
   }
 
+  fileSelector(event) {
+    this.fileName = event.srcElement.files[0].name;
+    this.xmlFileName = this.fileName;
+    this.onGetFile();
+  }
   onGetFile() {
     this.automatedUIServices
       .getConfig$(this.configPath, this.xmlFileName)
@@ -106,7 +110,10 @@ export class ConfigurationsComponent implements OnInit {
               break;
             }
             case 400: {
-              this.toastr.error("Check Your Field Inputs", "An Error Occurred!");
+              this.toastr.error(
+                "Check Your Field Inputs",
+                "An Error Occurred!"
+              );
               break;
             }
             case 500: {
@@ -125,37 +132,37 @@ export class ConfigurationsComponent implements OnInit {
   }
   onUpdate(configData) {
     this.automatedUIServices
-    .putConfig$(this.formData, this.configPath, this.xmlFileName)
-    .subscribe(
-      data => {
-        this.toastr.success("Success!");
-      },
-      error => {
-        switch (error.status) {
-          case 404: {
-            this.toastr.error("File Not Found", "An Error Occured!");
-            break;
-          }
-          case 409: {
-            this.toastr.error("File Must be Specified!", "An Error Occurred!");
-            break;
-          }
-          case 400: {
-            this.toastr.error("Bad Request!", "An Error Occurred!");
-            break;
-          }
-          case 500: {
-            this.toastr.error("Internal Server Error!");
-            break;
-          }
-          default: {
-            this.toastr.error(
-              "File Was Not Updated!",
-              "An Error Occurred!"
-            );
+      .putConfig$(this.formData, this.configPath, this.xmlFileName)
+      .subscribe(
+        data => {
+          this.toastr.success("Success!");
+        },
+        error => {
+          switch (error.status) {
+            case 404: {
+              this.toastr.error("File Not Found", "An Error Occured!");
+              break;
+            }
+            case 409: {
+              this.toastr.error(
+                "File Must be Specified!",
+                "An Error Occurred!"
+              );
+              break;
+            }
+            case 400: {
+              this.toastr.error("Bad Request!", "An Error Occurred!");
+              break;
+            }
+            case 500: {
+              this.toastr.error("Internal Server Error!");
+              break;
+            }
+            default: {
+              this.toastr.error("File Was Not Updated!", "An Error Occurred!");
+            }
           }
         }
-      }
-    );
-}
+      );
+  }
 }
