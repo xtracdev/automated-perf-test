eeaa7e9138415e12dcf184e4b3669ebbe9b29c66
Farcas, Camelia <camelia.farcas@fmr.com>
2018-03-05 16:09:31 +0000

    XAPTC-63 delete all test cases (one file added)

--- uiServices/src/testCaseServices_test.go
+++ uiServices/src/testCaseServices_test.go
@@ -49,26 +49,6 @@ func TestGetAllCasesNoHeader(t *testing.T) {
 	}
 }
 
-//func TestSuccessfulGetTestCase(t *testing.T) {
-//	r := chi.NewRouter()
-//	r.Mount("/", GetIndexPage())
-//
-//	filePath := os.Getenv("GOPATH") + "/src/github.com/xtracdev/automated-perf-test/uiServices/test/"
-//	request, err := http.NewRequest(http.MethodGet, "/test-cases/Case1", nil)
-//
-//	request.Header.Set("testCasePathDir", filePath)
-//	request.Header.Get("testCasePathDir")
-//
-//	w := httptest.NewRecorder()
-//	r.ServeHTTP(w, request)
-//
-//	if err != nil {
-//		t.Error(err)
-//	}
-//
-//	assert.Equal(t,http.StatusOK, w.Code, "Error. Did not successfully GET")
-//}
-
 func TestGetTestCaseNoHeader(t *testing.T) {
 	r := chi.NewRouter()
 	r.Mount("/", GetIndexPage())
